659a40dbcee8819a64df762bc48bfdc12f037bfa
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-07-16 12:42:06 -0400

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,6 @@
-const tones = Array.from({ length: 10 }).reduce((obj, val, index) => {
-    const tone = index == 0 ? "50" : String(index * 100);
-    const tones = Array.from({ length: 10 }).reduce((_obj, _val, _index) => {
-      const _tone = String(_index) + (_index != 0 ? "0" : "");
-      _obj[tone + "/" + _tone] = `var(--${tone}-${_tone})`;
-      return _obj;
-    }, {});
-    obj = {
-      ...tones,
-      ...obj
-    };
-    return obj;
+const tones = Array.from({ length: 10 }, (_, i) => i * 100).reduce((tones, tone) => {
+    tones[tone] = `var(--color-${tone})`;
+    return tones;
 }, {});
 
 module.exports = {
@@ -23,58 +14,10 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                amber: {
-                    ...tones,
-                    900: 'var(--900)',
-                    800: 'var(--800)',
-                    700: 'var(--700)',
-                    600: 'var(--600)',
-                    500: 'var(--500)',
-                    400: 'var(--400)',
-                    300: 'var(--300)',
-                    200: 'var(--200)',
-                    100: 'var(--100)',
-                    50: 'var(--50)',
-                },
-                red: {
-                    ...tones,
-                    900: 'var(--900)',
-                    800: 'var(--800)',
-                    700: 'var(--700)',
-                    600: 'var(--600)',
-                    500: 'var(--500)',
-                    400: 'var(--400)',
-                    300: 'var(--300)',
-                    200: 'var(--200)',
-                    100: 'var(--100)',
-                    50: 'var(--50)',
-                },
-                cyan: {
-                    ...tones,
-                    900: 'var(--900)',
-                    800: 'var(--800)',
-                    700: 'var(--700)',
-                    600: 'var(--600)',
-                    500: 'var(--500)',
-                    400: 'var(--400)',
-                    300: 'var(--300)',
-                    200: 'var(--200)',
-                    100: 'var(--100)',
-                    50: 'var(--50)',
-                },
-                yellow: {
-                    ...tones,
-                    900: 'var(--900)',
-                    800: 'var(--800)',
-                    700: 'var(--700)',
-                    600: 'var(--600)',
-                    500: 'var(--500)',
-                    400: 'var(--400)',
-                    300: 'var(--300)',
-                    200: 'var(--200)',
-                    100: 'var(--100)',
-                    50: 'var(--50)',
-                }
+                amber: tones,
+                red: tones,
+                cyan: tones,
+                yellow: tones
             },
         }
     },
